a5db335eb3bef0a1263b5698f25cf6b0b88f717f
Sophie Ye <sophixer@gmail.com>
2014-01-22 15:41:44 -0800

    made the page look nice!

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,50 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("j/k it's actually Sophie's page!");
+		$('#testjs').text("Ahhh, you clicked me!");
+		$(".jumbotron p").toggleClass("active");
 	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+	$("#submitBtn").click(adjustProject);
+}
+
+function adjustProject() {
+	var projectID = $("#project").val();
+	var newWidth = $("#width").val();
+	var newDesc = $("#description").val();
+
+	var projectToChange = $(projectID);
+	var descToChange = $(projectToChange).find(".project-description");
+
+	projectToChange.width(newWidth);
+	descToChange.text(newDesc);
+}
+
+function projectClick(e) {
+	// Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the element that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    var image = $(containingProject).find(".img");
+
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>This project has a placeholder description for now. Go ahead and change it at the bottom of the page!</p></div>");
+    } else {
+    	if (description.is(":visible")) {
+	    	description.fadeOut();
+	    	image.fadeOut();
+	    } else {
+	       description.fadeIn();
+	       image.fadeIn();
+	    }
+	}
 }
